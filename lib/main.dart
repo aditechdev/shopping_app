a174d25bb130f7ad41529e5f9dedf766307db736
aditechdev <aditechdevofficial@gmail.com>
2022-06-10 19:38:38 +0530

    :iphone: COlors added

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Shopping App Clone ',
       theme: ThemeData(
        
         primarySwatch: Colors.blue,
