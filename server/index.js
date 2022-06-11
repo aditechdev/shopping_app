9e99749637d09de2c3e0c747fa23e0089dc2ab03
aditechdev <aditechdevofficial@gmail.com>
2022-06-11 10:00:12 +0530

    :sparkles: User Mongoose schema and model set up

--- server/index.js
+++ server/index.js
@@ -14,6 +14,7 @@ const PORT = 3000; //localHost
 
 //! MiddleWare
 //! Client -> Server -> Clinet
+app.use(express.json());
 app.use(authRouter);
 
 // ! Connect database =>
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,15 +1,45 @@
 const express = require("express");
+const User = require("../models/user");
 
 const authRouter = express.Router();
+
 // API
-authRouter.post("/api/signup", (req, res) => { 
+authRouter.post("/api/signup", async (req, res) => { 
 
-    // Get Data 
-    const {name, email, password} = req.body;
+    try {
+         // Get Data 
+    const { name, email, password } = req.body;
+    // TO check existing user with same email id
+    const existingUser = await User.findOne({ email });
 
-    // Post Data in database
+    if (existingUser) {
+        return res.status(400).json({
+            msg: "User with same email already exist!"
+        });
+        
+    }
 
+    let user = new User({
+        email,
+        password,
+        name,
+    })
+
+    // save the data 
+    user = await user.save();
     // Return that Data to client
+    res.json(user)
+        
+    } catch (error) {
+        res.status(500).json({
+            error: error.message
+        })
+        
+    }
+
+   ;
+
+    
 })
 // Exports the Route
 module.exports = authRouter;
