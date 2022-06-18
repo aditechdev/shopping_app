3c5951c832b0ee2e21947a067aae36cf55441cc7
aditechdev <aditechdevofficial@gmail.com>
2022-06-19 01:25:49 +0530

    :sparkles: Dotted border Form Field done

--- lib/features/admin/screen/add_product_screen.dart
+++ lib/features/admin/screen/add_product_screen.dart
@@ -1,3 +1,4 @@
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:shopping_app/constants/global_variable.dart';
 
@@ -13,21 +14,63 @@ class _AddProductScreenState extends State<AddProductScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(50),
-          child: AppBar(
-              flexibleSpace: Container(
-                decoration: const BoxDecoration(
-                  gradient: GlobalVariable.appBarGradient,
-                ),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(50),
+        child: AppBar(
+            flexibleSpace: Container(
+              decoration: const BoxDecoration(
+                gradient: GlobalVariable.appBarGradient,
               ),
-              title: const Text(
-                "Add Product",
-                style: TextStyle(
-                  color: Colors.black,
+            ),
+            title: const Text(
+              "Add Product",
+              style: TextStyle(
+                color: Colors.black,
+              ),
+            )),
+      ),
+      body: SingleChildScrollView(
+        child: Form(
+            child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              DottedBorder(
+                borderType: BorderType.RRect,
+                radius: const Radius.circular(10),
+                dashPattern: const [10, 4],
+                strokeCap: StrokeCap.round,
+                child: Container(
+                  width: double.infinity,
+                  height: 150,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Icon(
+                        Icons.folder_open,
+                        size: 40,
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        "Select Product Images",
+                        style: TextStyle(fontSize: 14, color: Colors.grey[400]),
+                      ),
+                    ],
+                  ),
                 ),
-              )),
-        ),
-        body: const Text("Add Product"));
+              )
+            ],
+          ),
+        )),
+      ),
+    );
   }
 }
