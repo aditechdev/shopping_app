7c250aa52907a368a0faaf830ca02c1995721f25
aditechdev <aditechdevofficial@gmail.com>
2022-06-19 00:00:17 +0530

    :tada: Introduce Admin Screen

---
+++ lib/features/admin/screen/admin_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AdminScreen extends StatefulWidget {
+  const AdminScreen({ Key? key }) : super(key: key);
+
+  @override
+  State<AdminScreen> createState() => _AdminScreenState();
+}
+
+class _AdminScreenState extends State<AdminScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shopping_app/common/widget/bottom_bar.dart';
 import 'package:shopping_app/constants/global_variable.dart';
+import 'package:shopping_app/features/admin/screen/admin_screen.dart';
 import 'package:shopping_app/features/auth/screens/auth_screens.dart';
 import 'package:shopping_app/features/auth/services/auth_service.dart';
 import 'package:shopping_app/provider/user_provider.dart';
@@ -56,7 +57,7 @@ class _MyAppState extends State<MyApp> {
       ),
       onGenerateRoute: (settings) => generateRoute(settings),
       home: Provider.of<UserProvider>(context).user.token.isNotEmpty
-          ? const BottomBar()
+          ? (Provider.of<UserProvider>(context).user.type == "user") ? const BottomBar() : const AdminScreen()
           : const AuthScreen(),
     );
   }
