17b960d7f92b7e93914274c17a82a0ec24cfd66e
aditechdev <aditechdevofficial@gmail.com>
2022-06-18 11:34:55 +0530

    :sparkles: Home Appbar design

--- lib/features/home/screens/home_screens.dart
+++ lib/features/home/screens/home_screens.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shopping_app/constants/global_variable.dart';
 import 'package:shopping_app/provider/user_provider.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -15,6 +16,72 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     final user = Provider.of<UserProvider>(context).user;
     return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(50),
+        child: AppBar(
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+              gradient: GlobalVariable.appBarGradient,
+            ),
+          ),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Container(
+                  height: 42,
+                  margin: const EdgeInsets.only(left: 15),
+                  child: Material(
+                    elevation: 1,
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(
+                        7,
+                      ),
+                    ),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                          prefixIcon: InkWell(
+                            onTap: () {},
+                            child: const Padding(
+                              padding: EdgeInsets.only(left: 6),
+                              child: Icon(
+                                Icons.search,
+                                color: Colors.black,
+                                size: 23,
+                              ),
+                            ),
+                          ),
+                          fillColor: Colors.white,
+                          filled: true,
+                          contentPadding: const EdgeInsets.only(top: 7),
+                          border: const OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(
+                                7,
+                              ),
+                            ),
+                            borderSide: BorderSide.none,
+                          ),
+                          enabledBorder: const OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(
+                                7,
+                              ),
+                            ),
+                            borderSide: BorderSide(
+                              color: Colors.black38,
+                              width: 1,
+                            ),
+                          ),
+                          hintText: "Search product"),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
       body: Center(
         child: Text(user.toJson()),
       ),
