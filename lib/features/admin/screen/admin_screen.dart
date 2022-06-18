fa8d0c905ec4a025357094e5e0a889d4100bff20
aditechdev <aditechdevofficial@gmail.com>
2022-06-19 00:07:35 +0530

    :sparkles: Admin App bar done

--- lib/features/admin/screen/admin_screen.dart
+++ lib/features/admin/screen/admin_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:shopping_app/constants/global_variable.dart';
 
 class AdminScreen extends StatefulWidget {
-  const AdminScreen({ Key? key }) : super(key: key);
+  const AdminScreen({Key? key}) : super(key: key);
 
   @override
   State<AdminScreen> createState() => _AdminScreenState();
@@ -11,9 +13,38 @@ class _AdminScreenState extends State<AdminScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(60),
+        child: AppBar(
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+              gradient: GlobalVariable.appBarGradient,
+            ),
+          ),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                alignment: Alignment.topLeft,
+                child: SvgPicture.asset(
+                  "assets/logo/appbar_logo.svg",
+                  width: 120,
+                  height: 45,
+                  color: Colors.black,
+                ),
+              ),
+              const Text(
+                "ADMIN",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              )
+            ],
+          ),
+        ),
       ),
+      body: Container(),
     );
   }
 }
