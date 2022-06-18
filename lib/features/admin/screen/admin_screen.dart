5374541807b408b251655a5bf155d395fb8229ba
aditechdev <aditechdevofficial@gmail.com>
2022-06-19 00:17:37 +0530

    :wastebasket: Remove Unused Imports

--- lib/features/admin/screen/admin_screen.dart
+++ lib/features/admin/screen/admin_screen.dart
@@ -1,4 +1,3 @@
-import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:shopping_app/constants/global_variable.dart';
